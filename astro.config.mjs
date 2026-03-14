ff9a6d9b5a04d29bd58791106c84c2ee3869002b
Lenin Chavez <lg.chavez1404@gmail.com>
2026-03-14 18:35:56 -0500

    feat: Add Vercel adapter for deployment.

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,10 +5,12 @@ import node from "@astrojs/node";
 import react from "@astrojs/react";
 import sitemap from "@astrojs/sitemap";
 
+import vercel from "@astrojs/vercel";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://bylenz.com",
-  adapter: node({ mode: "standalone" }),
+  adapter: vercel(),
   vite: {
     plugins: [tailwindcss()],
   },
